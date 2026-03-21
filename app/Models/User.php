007c1e02340bcee30f2deae99a2211c35b2cc540
Landslidez <erichpramana88@gmail.com>
2026-03-21 22:56:22 +0700

    Cek register 5

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,9 @@ class User extends Authenticatable
     protected $fillable = [
         'nama',
         'email',
-        'password',
+        'pin',
+        'no_hp',
+        'role',
     ];
 
     /**
