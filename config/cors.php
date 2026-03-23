341770942b9db4d38b215fd709231c8f9d43aa4c
AiraCode <williamharyanto34@gmail.com>
2026-03-23 22:14:24 +0700

    ssss

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://easykos.vercel.app'], // Port Next.js kamu
+    'allowed_origins' => ['https://easykos.vercel.app', 'http://192.168.18.199:3000/'], // Port Next.js kamu
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
