9af8da0173fc7ad2d7a77767e5e76b4b987ad37c
AiraCode <williamharyanto34@gmail.com>
2026-03-23 21:46:26 +0700

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://easykosbackend-production.up.railway.app/'], // Port Next.js kamu
+    'allowed_origins' => ['http://easykos.vercel.app'], // Port Next.js kamu
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
