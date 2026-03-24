9c4bfa0c00fe2a3e150d56165a1ff8a03f60e50a
AiraCode <williamharyanto34@gmail.com>
2026-03-24 15:05:38 +0700

    Update ProfileController.php

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -63,7 +63,7 @@ class ProfileController extends Controller
         }
 
         if ($request->has('name')) {
-            $user->nama = $request->name;
+            $user->name = $request->name;
         }
         if ($request->has('no_hp')) {
             $user->no_hp = $request->no_hp;
