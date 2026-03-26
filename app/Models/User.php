5618722a1cf723a70b65d521b15d86fa31d281cb
KevinMargason <s160423014@student.ubaya.ac.id>
2026-03-26 17:29:11 +0700

    Gaktau

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,6 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable;
     /** @use HasFactory<UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
 
